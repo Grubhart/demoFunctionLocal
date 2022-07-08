12ae35f398459a38dd216cd2c895cabcccfd8429
Grubhart <grubhart@gmail.com>
2022-07-08 13:53:05 -0500

    first commit

---
+++ HttpTrigger_CIX_DEMO_Local.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class HttpTrigger_CIX_DEMO_Local
+    {
+        [FunctionName("HttpTrigger_CIX_DEMO_Local")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "Trigger ejecutado con Exito ingresa un nombre para una respuesta personalizada."
+                : $"Hola, {name}. Funcion ejecutada con Exito";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
